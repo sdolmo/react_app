508b98f6101023dbfc52f839aafcd7de40ca4b10
Sylvia <sylviad1221@gmail.com>
2017-04-03 23:58:44 -0400

    add react router

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-// code here
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import './css/style.css';
+import StorePicker from './components/StorePicker';
+import App from './components/App';
+import NotFound from './components/NotFound';
+
+const Root = () => {
+  return (
+    <Router>
+      <div>
+        <Route exact path='/' component={StorePicker} />
+        <Route path='/store/:storeId' component={App} />
+      </div>
+    </Router>
+  )
+}
+
+render(<Root/>, document.querySelector('#root'));
